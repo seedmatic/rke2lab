253926eb0d3f372a34874af418b8f7c959152922
rke2lab:manifests-render <rke2lab+manifests-render@mammoth-skate.ts.net>
2026-09-14 19:09:01 +0200

    render bioskop-mgmt manifests @ 048bd59edfb5

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
                 yq eval -o=yaml                   '(.data // {}) | with_entries(.value |= from_yaml)'                   "$manifest" > "$dest/$name"
                 count=$((count + 1))
               done < <(find "${self}" -type f \( -name '*.yaml' -o -name '*.yml' \) -print0)
-              echo "[install-rke2-config] installed $count RKE2_CONFIG fragment(s) for"                    "cluster $cluster into $dest"
+              echo "[install-rke2-config] installed $count fragment(s) for cluster $cluster into $dest"
             '';
           };
           app = { type = "app"; program = "${installer}/bin/install-rke2-config"; };
